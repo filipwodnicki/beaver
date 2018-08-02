a5bd72151d51851a4e5a4b2d6df04f174b888e4a
bananalytics <31481575+bananalytics@users.noreply.github.com>
2018-08-02 23:23:26 +0200

    added algorithm

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 from app.forms import WoodcutForm
-from flask import render_template, redirect, url_for, request
+from app.woodcut.algorithms.greedy import greedy_algorithm
+from flask import render_template, redirect, url_for, request, jsonify
 
 @app.route('/')
 @app.route('/index')
@@ -8,8 +9,23 @@ def index():
 	form = WoodcutForm()
 	return render_template('index.html', form=form)
 
-@app.route('/success', methods=['GET','POST'])
-def success():
+# @app.route('/success', methods=['GET','POST'])
+# def success():
+# 	material_size = request.form.get('material_size')
+# 	item_list = request.form.get('item_list')
+# 	return render_template( 'success.html', material_size=material_size, item_list=item_list)
+
+@app.route('/cut', methods=['POST'])
+def cut():
 	material_size = request.form.get('material_size')
-	item_list = request.form.get('item_list')
-	return render_template( 'success.html', material_size=material_size, item_list=item_list)
+	item_string = request.form.get('item_list')
+	item_list_of_string = item_string.split(',')
+	item_list_of_float = [float(i) for i in item_list_of_string]
+	# print(item_list_of_float)
+
+	statement = ""
+	for board in greedy_algorithm(item_list_of_float):
+		statement += str( board )
+	print ( statement )
+	return jsonify( { "data" : statement } )
+	# return "Not yet ;)"
---
+++ app/woodcut/algorithms/greedy.py
@@ -0,0 +1,34 @@
+from app.woodcut.board import Board
+
+
+
+def greedy_algorithm( pieces ):
+
+    board_collection = []
+
+    # Algorithm
+
+    board_collection.append(Board())
+
+    pieces.sort(reverse=True)  # sort in ascending order
+
+    def try_placing(piece):
+        for board in board_collection:
+            if board.space_remaining >= piece:
+                board.insert(piece)
+                pieces.remove(piece)
+                return True
+        return False
+
+    for piece in pieces[:]:  # pieces[:] is a copy for the purposes of the algorithm, otherwise our pieces[] would get modified
+
+        #     print ( board_collection[0].contents )
+        if try_placing(piece) == True:
+            pass
+        else:
+            board_collection.append(Board())
+            board_collection[-1].insert(piece)
+            pieces.remove(piece)
+
+
+    return str( board_collection )
