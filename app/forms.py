3f808beda3655e39f34730bb0a166f1d094c281c
bananalytics <31481575+bananalytics@users.noreply.github.com>
2018-08-06 01:08:50 +0200

    minor deployment changes

--- app/forms.py
+++ app/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired
 
 class WoodcutForm(FlaskForm):
 	material_size = FloatField('Material Size', validators=[DataRequired()], default=2050.0)
-	item_list = StringField('Pieces you need', validators=[DataRequired()], default='540, 450, 389')
+	item_list = StringField('Pieces you need', validators=[DataRequired()], default='450, 444, 436, 430, 389, 389, 386, 375, 362, 362, 261, 261, 261')
 	submit = SubmitField('Calculate')
 
 	# make custom validator that makes sure we have a list of integers or floats in item_list
