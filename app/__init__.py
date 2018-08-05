4d5bf2abbb3e742dd950a08e2e9510eb5808d368
bananalytics <31481575+bananalytics@users.noreply.github.com>
2018-08-05 20:07:37 +0200

    added AJAX feature

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'you-will-never-guess'
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
+from app.woodcut.algorithms.greedy import greedy_algorithm
+from flask import render_template, redirect, url_for, flash, jsonify
 from app import app
 from app.forms import WoodcutForm
-from app.woodcut.algorithms.greedy import greedy_algorithm
-from flask import render_template, redirect, url_for, request, jsonify
 
 @app.route('/')
 @app.route('/index')
@@ -9,23 +9,17 @@ def index():
 	form = WoodcutForm()
 	return render_template('index.html', form=form)
 
-# @app.route('/success', methods=['GET','POST'])
-# def success():
-# 	material_size = request.form.get('material_size')
-# 	item_list = request.form.get('item_list')
-# 	return render_template( 'success.html', material_size=material_size, item_list=item_list)
-
-@app.route('/cut', methods=['POST'])
+@app.route('/cut', methods=['post'])
 def cut():
-	material_size = request.form.get('material_size')
-	item_string = request.form.get('item_list')
-	item_list_of_string = item_string.split(',')
-	item_list_of_float = [float(i) for i in item_list_of_string]
-	# print(item_list_of_float)
-
-	statement = ""
-	for board in greedy_algorithm(item_list_of_float):
-		statement += str( board )
-	print ( statement )
-	return jsonify( { "data" : statement } )
-	# return "Not yet ;)"
+	form = WoodcutForm()
+	if form.validate_on_submit():
+		material_size = form.material_size.data
+		item_string = form.item_list.data
+		item_list_of_string = item_string.split(',')
+		item_list_of_float = [float(i) for i in item_list_of_string]
+		statement = ""
+		for board in greedy_algorithm(item_list_of_float):
+			statement += str( board )
+		print ( statement )
+		return jsonify({ "payload" : statement })
+	return jsonify({'payload':"couldn\'t submit anything"})
